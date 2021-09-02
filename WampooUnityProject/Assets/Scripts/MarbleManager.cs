8f695944662cda2c813c019353824685f87d1900
Randy Herritt <randy.herritt@gmail.com>
2021-09-02 19:40:27 -0300

    Added Marbles for each player
    Added marbles in starting positions for each player with colours red, green, yellow, and blue.

---
+++ WampooUnityProject/Assets/Scripts/MarbleManager.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MarbleManager : MonoBehaviour
+{
+    public Material material;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetComponent<Renderer>().material = material;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
