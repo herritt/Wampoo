05fc09daa84c166564e121ba528088f9f99f6a75
Randy Herritt <randy.herritt@gmail.com>
2021-09-11 22:24:20 -0300

    Finished determining who goes first
    First player to be dealt a spade goes first.

--- WampooUnityProject/Assets/Scripts/CardManager.cs
+++ WampooUnityProject/Assets/Scripts/CardManager.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -16,7 +17,6 @@ public class CardManager : MonoBehaviour
 
     private GameObject currentCardBeingDelt;
     private GameObject currentTarget;
-    private bool sendingCardToPlayer = false;
     private bool determingingWhoToGoFirst = false;
     private bool cardFlipped = false;
     private int currentPlayerBeingDelt;
@@ -24,36 +24,49 @@ public class CardManager : MonoBehaviour
     private Quaternion rotateTo;
     private float halfway;
 
+    private Hashtable suitMap;
+
+    private enum Suit { Hearts, Diamonds, Spades, Clubs, Joker };
 
     // Start is called before the first frame update
     void Start()
     {
         deck = new GameObject[DECK_SIZE];
+        suitMap = new Hashtable();
 
         for (int i = 0; i < DECK_SIZE; i++)
         {
             GameObject cardObject = Instantiate(cardPrefab, new Vector3(0, 0, 0), Quaternion.identity);
             Card card = cardObject.GetComponent<Card>();
-            card.Init(i, textures[i]);
+            Texture texture = textures[i];
+            string name = texture.name;
+
+            suitMap.Add(i, SuitFromName(name));
+                 
+            card.Init(i, texture);
             cardObject.transform.Rotate(90, 0, 0);
             deck[i] = cardObject;
         }
 
         ShuffleDeck();
         StackDeck();
+    }
 
-        foreach (GameObject cardObject in deck)
-        {
-            Card card = cardObject.GetComponent<Card>();
-            Debug.Log(card.id);
-        }
+    private Suit SuitFromName(string name)
+    {
+        if (name.Contains("hearts")) return Suit.Hearts;
+        if (name.Contains("diamonds")) return Suit.Diamonds;
+        if (name.Contains("clubs")) return Suit.Clubs;
+        if (name.Contains("joker")) return Suit.Joker;
+
+        return Suit.Spades;       
     }
 
     void ShuffleDeck()
     {
         for (int i = 0; i < DECK_SIZE; i++)
         {
-            int randomIndex = Random.Range(0, DECK_SIZE);
+            int randomIndex = UnityEngine.Random.Range(0, DECK_SIZE);
             GameObject cardObject = deck[randomIndex];
             deck[randomIndex] = deck[i];
             deck[i] = cardObject;
@@ -64,12 +77,12 @@ public class CardManager : MonoBehaviour
     {
         for (int i = 0; i < DECK_SIZE; i++)
         {
-            float jitter = Random.Range(0, 1f);
+            float jitter = UnityEngine.Random.Range(0, 1f);
 
             GameObject cardObject = deck[i];
             cardObject.transform.Translate(new Vector3(jitter, jitter, -CARD_THICKNESS - (i * CARD_THICKNESS)));
 
-            jitter = Random.Range(-2, 2f);
+            jitter = UnityEngine.Random.Range(-2, 2f);
             cardObject.transform.Rotate(0, 0, jitter);
 
         }
@@ -80,7 +93,6 @@ public class CardManager : MonoBehaviour
         currentCardBeingDelt = deck[dealCounter--];
         currentPlayerBeingDelt = (currentPlayerBeingDelt + 1) % 4;
         currentTarget = dealPositions[currentPlayerBeingDelt];
-        sendingCardToPlayer = true;
 
         halfway = Vector3.Distance(currentCardBeingDelt.transform.position, currentTarget.transform.position) / 2f;
 
@@ -92,17 +104,14 @@ public class CardManager : MonoBehaviour
             rotateTo = currentCardBeingDelt.transform.rotation * Quaternion.Euler(rotationVector);
 
         }
-
-
-       
+ 
     }
 
     public void DetermineWhoGoesFirst()
     {
         dealCounter = DECK_SIZE - 1;
-        currentPlayerBeingDelt = Random.Range(1, 5);
+        currentPlayerBeingDelt = UnityEngine.Random.Range(1, 5);
         determingingWhoToGoFirst = true;
-        sendingCardToPlayer = true;
         DealNextCard(true);
 
         //if its not a spade, go to next person
@@ -110,9 +119,15 @@ public class CardManager : MonoBehaviour
         //if it is a spade, this person goes first - let game manager know
     }
 
-    private void CheckIfSpade()
+    private bool CheckIfSpade()
     {
-        DealNextCard(true);
+        Card card = currentCardBeingDelt.GetComponent<Card>();
+        int id = card.id;
+
+        Suit suit = (Suit)suitMap[id];
+        if (suit.Equals(Suit.Spades)) return true;
+
+        return false;
 
     }
     // Update is called once per frame
@@ -121,39 +136,38 @@ public class CardManager : MonoBehaviour
 
         if (determingingWhoToGoFirst)
         {
-            if (sendingCardToPlayer)
+            int offset = (DECK_SIZE - dealCounter);
+            float heightOfCardAtTarget = (CARD_THICKNESS + (offset * CARD_THICKNESS));
+            currentTarget.transform.position = new Vector3(currentTarget.transform.position.x, heightOfCardAtTarget, currentTarget.transform.position.z);
+
+            currentCardBeingDelt.transform.position = Vector3.Lerp(currentCardBeingDelt.transform.position,
+                currentTarget.transform.position, Time.deltaTime * dealSpeed);
+
+            float distance = Vector3.Distance(currentCardBeingDelt.transform.position, currentTarget.transform.position);
+
+            if (cardFlipped && distance < halfway)
             {
-                int offset = (DECK_SIZE - dealCounter);
-                float heightOfCardAtTarget = (CARD_THICKNESS + (offset * CARD_THICKNESS));
-                currentTarget.transform.position = new Vector3(currentTarget.transform.position.x, heightOfCardAtTarget, currentTarget.transform.position.z );
-
-                currentCardBeingDelt.transform.position = Vector3.Lerp(currentCardBeingDelt.transform.position,
-                    currentTarget.transform.position, Time.deltaTime * dealSpeed);
-
-                float distance = Vector3.Distance(currentCardBeingDelt.transform.position, currentTarget.transform.position);
-
-                if (cardFlipped && distance < halfway)
-                {
-
-                    currentCardBeingDelt.transform.rotation = Quaternion.Lerp(currentCardBeingDelt.transform.rotation, rotateTo, Time.deltaTime * dealSpeed * 4);
-                }
-                else
-                {
-                    currentCardBeingDelt.transform.position = new Vector3(currentCardBeingDelt.transform.position.x, 
-                        DECK_SIZE * CARD_THICKNESS, 
-                        currentCardBeingDelt.transform.position.z);
-                }
-
-                if (Vector3.Distance(currentCardBeingDelt.transform.position, currentTarget.transform.position) < EPISOLON)
-                {
-                    sendingCardToPlayer = false;
-                    cardFlipped = false;
-                }
-
+                currentCardBeingDelt.transform.rotation = Quaternion.Lerp(currentCardBeingDelt.transform.rotation, rotateTo, Time.deltaTime * dealSpeed * 4);
             }
             else
             {
-                CheckIfSpade();
+                currentCardBeingDelt.transform.position = new Vector3(currentCardBeingDelt.transform.position.x,
+                    DECK_SIZE * CARD_THICKNESS,
+                    currentCardBeingDelt.transform.position.z);
+            }
+
+            if (Vector3.Distance(currentCardBeingDelt.transform.position, currentTarget.transform.position) < EPISOLON)
+            {
+                if (CheckIfSpade())
+                {
+                    
+                    Debug.Log("player " + (currentPlayerBeingDelt + 1) + " goes first");
+                }
+                else
+                {
+                    DealNextCard(true);
+                }
+                 
             }
 
         }
